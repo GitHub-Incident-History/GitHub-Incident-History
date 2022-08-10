f7ba3a4eacc6312a701557ddd58a4e1782429131
PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-08-10 15:22:48 +0100

    correct data by deleting maintanance records

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def download_all_incident_codes():
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     }
-    for page in range(1, 52):
+    for page in tqdm(range(1, 52)):
         url = f'https://www.githubstatus.com/history?page={page}'
         print(url)
         response = requests.get(url, headers=headers)
@@ -24,7 +24,9 @@ def download_all_incident_codes():
                 #     'impact': 'minor', 
                 #     'timestamp': "Aug <var data-var='date'>10</var>, <var data-var='time'>00:17</var> - <var data-var='time'>01:30</var> UTC"
                 # }
-                incident_codes.append(incident['code'])
+                # Exclude all maintenances
+                if (incident['impact'] != 'maintenance'):
+                    incident_codes.append(incident['code'])
     with open('incident_codes.json', 'w') as file:
         json.dump(incident_codes, file)
 
@@ -49,11 +51,30 @@ def download_all_incident_records():
         with open(f'incidents/{incident_code}.json', 'w') as file:
             json.dump(incident, file, indent=4)
 
+def read_incident(id: str):
+    with open(f'incidents/{id}.json', 'r') as file:
+        incident = json.load(file)
+    return incident
+
+
+def create_incident_commits():
+    incident_codes = read_incident_codes()
+    incidents = []
+    for incident_code in incident_codes:
+        incident = read_incident(incident_code)
+        if 'incident' not in incident:
+            print(incident_code)
+            exit(0)
+        incidents.append(incident)
+    # print(incidents[0]['incident']['created_at'])
+    incidents.sort(key=lambda x:x['incident']['created_at'])
+    print(len(incidents))
 def main():
     # incident_codes = read_incident_codes()
     # print(len(incident_codes))
     # download_all_incident_codes()
     download_all_incident_records()
+    # create_incident_commits()
 
 
 if __name__ == '__main__':
